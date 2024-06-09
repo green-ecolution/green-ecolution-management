2748c50e2f8e550052aa006bf24bde3f0aa1bcfa
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-06-09 20:01:44 +0200

    chore: init golang project and implement basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SmartCityFlensburg/green-space-management
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
