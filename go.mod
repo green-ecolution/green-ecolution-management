8cb385db8a011517e8cf755dc77424b537b442ca
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-06-09 20:11:34 +0200

    feat: add config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/SmartCityFlensburg/green-space-management
 
 go 1.22.4
+
+require (
+	github.com/caarlos0/env/v11 v11.0.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
 )
 
+type Config struct {
+	Port int `env:"PORT" envDefault:"8000"`
+}
+
 func main() {
+	godotenv.Load()
+
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
 
-	http.ListenAndServe(":8000", nil)
+	fmt.Printf("Starting server on port %d\n", cfg.Port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil))
 }
