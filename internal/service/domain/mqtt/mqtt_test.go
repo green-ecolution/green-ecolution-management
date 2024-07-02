9a42a7a5d1222824bf9f92406c0ab680a681e67f
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-07-02 23:53:16 +0200

    fix: fix test

--- internal/service/domain/mqtt/mqtt_test.go
+++ internal/service/domain/mqtt/mqtt_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	storageMock "github.com/SmartCityFlensburg/green-space-management/internal/storage/_mock"
 )
 
 func TestNewMqttService(t *testing.T) {
+  repo := storageMock.NewMockMqttRepository(t)
 	t.Run("should create a new service", func(t *testing.T) {
-		svc := NewMqttService()
+		svc := NewMqttService(repo)
 		assert.NotNil(t, svc)
 	})
 }
