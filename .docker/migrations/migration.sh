836090c44d179ef835cc0e96864ec48db34721c0
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-08-28 01:37:30 +0200

    ci: add seed and reset db on start

--- .docker/migrations/migration.sh
+++ .docker/migrations/migration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "Resetting database"
+goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" down
+goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" down
+
+echo "Running migration"
 goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" up
+
+echo "Applying seed"
+goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" up
