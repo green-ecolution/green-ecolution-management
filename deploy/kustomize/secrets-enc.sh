d4d8022122b3aa095a726e57621cacd05bb76e75
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-08-28 00:03:43 +0200

    ci: change kustomize, remove mongodb and add migrations in initContainer

--- deploy/kustomize/secrets-enc.sh
+++ deploy/kustomize/secrets-enc.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-sops -e ./components/mongodb/secrets.yaml > ./components/mongodb/secrets.enc.yaml
-sops -e ./components/mongodb/mongo-express/secrets.yaml > ./components/mongodb/mongo-express/secrets.enc.yaml
-
 sops -e ./dev/secrets.yaml > ./dev/secrets.enc.yaml
