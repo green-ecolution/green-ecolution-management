86ee6fd40d455ff5fd5e6f3dc75decd6d37ac565
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-25 20:34:07 +0200

    fix: set migration down to reset

--- .docker/migrations/migration.sh
+++ .docker/migrations/migration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Resetting database"
-goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" down
-goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" down
+goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" reset
+goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" reset
 
 echo "Running migration"
 goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" up
