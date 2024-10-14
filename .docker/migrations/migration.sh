ab8fd00b015645c8be010d6617a4e64b021a6e5a
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-14 17:46:20 +0200

    feat: add args to migration script

--- .docker/migrations/migration.sh
+++ .docker/migrations/migration.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-echo "Resetting database"
-goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" reset
-goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" reset
+set -e
+MIGRATION_WITH_SEED=${MIGRATION_WITH_SEED:-false}
+MIGRATION_WITH_DB_RESET=${MIGRATION_WITH_DB_RESET:-false}
+
+if [ "$MIGRATION_WITH_DB_RESET" = "true" ]; then
+  echo "Resetting database"
+  goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" reset
+  goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" reset
+fi
 
 echo "Running migration"
 goose -dir /migrations postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" up
+
+if [ "$MIGRATION_WITH_SEED" = "false" ]; then
+    echo "Migration completed"
+    exit 0
+fi
 
 echo "Applying seed"
 goose -dir /seed -no-versioning postgres "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" up
